14104f9464ea80d74f403458403496290faad0de
Scorpion-239 <aum3@njit.edu>
2023-08-07 15:27:02 -0400

    Nevigation added

--- nextjs-blog/pages/buyers_persona.js
+++ nextjs-blog/pages/buyers_persona.js
@@ -13,21 +13,16 @@ const BuyersPersona = () => {
       </Head>
 
       <header>
-        <img src="/images/Apex_Athletics_logo.png" alt="Logo" className={styles.logo} />
+        <img src="images/Apex_Athletics_logo.png" alt="Logo" className="logo" />
         <nav>
           <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/brand_guide">Brand Guide</Link>
-            </li>
-            <li>
-              <Link href="/marketing_strategy">Marketing Strategy</Link>
-            </li>
-            <li>
-              <Link href="/buyers_persona">Buyer's Persona</Link>
-            </li>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/brand_guide">Brand Guide</Link></li>
+            <li><Link href="/marketing_strategy">Marketing Strategy</Link></li>
+            <li><Link href="/buyers_persona">Buyer's Persona</Link></li>
+            <li><Link href="/privacy_policy">Privacy Policy</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
           </ul>
         </nav>
       </header>
