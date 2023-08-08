e554574785e34f7065d1b23a2dbcaf1e7e75f030
Scorpion-239 <aum3@njit.edu>
2023-08-08 19:01:54 -0400

    Added privacy policy css

--- nextjs-blog/pages/marketing_strategy.js
+++ nextjs-blog/pages/marketing_strategy.js
@@ -11,7 +11,7 @@ const ApexAthletics = () => {
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/x-icon" href="favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Apex Athletics Home</title>
+        <title>Apex Athletics Marketing Strategy</title>
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
         />
